f9ae5e4d19c76175e5bdaaca6c57173cbb656707
Schmoller <steven.schmoll@gmail.com>
2014-11-09 14:28:37 +1100

    Updated buildcraft support to 6.1.5 * NOTE: This means previous versions of buildcraft will NOT work with this version * Update Tubes version to 2.0.5

--- src/main/java/schmoller/tubes/BuildcraftProxy.java
+++ src/main/java/schmoller/tubes/BuildcraftProxy.java
@@ -1,12 +1,12 @@
 package schmoller.tubes;
 
 import net.minecraftforge.fluids.FluidRegistry;
-import buildcraft.core.recipes.RefineryRecipeManager;
+import buildcraft.api.recipes.BuildcraftRecipeRegistry;
 
 public class BuildcraftProxy
 {
 	public BuildcraftProxy()
 	{
-		RefineryRecipeManager.INSTANCE.addRecipe(FluidRegistry.getFluidStack("fuel", 1), FluidRegistry.getFluidStack("plastic", 1), 8, 1);
+		BuildcraftRecipeRegistry.refinery.addRecipe("liquidPlastic", FluidRegistry.getFluidStack("fuel", 1), FluidRegistry.getFluidStack("plastic", 1), 8, 1);
 	}
 }
